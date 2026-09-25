d438d687a6139e0cde7084a00cc55d849311e8c2
aysuh pathak <ayushpathak9389328906@gmail.com>
2026-09-25 10:13:39 +0530

    DataType Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,25 @@
+//JavaScript dynamic type language hai
+// java statically type language hai
+
+//primitive
+// 7 types : String , Number , Boolean , null , Undefined ,Symbol , BigInt
+
+const score = 100;
+// const score2:number = 100  // This is ts code (hum datatype od datatype likhate hai ts main type safty ke liye )
+
+//Reference (Non primitive)
+// Array , Objects , Functions
+
+const heros =["shaktiman", "naagraj","doga"]
+
+let myobj ={
+    name: "aysuh",
+    age: 22,
+}
+
+const myfunction = function () {
+    console.log("hello world")
+}
+
+
+console.log(typeof heros);
