37024766c850bb93877f56c2e211c90565ee0ac5
aysuh pathak <ayushpathak9389328906@gmail.com>
2026-09-25 09:33:05 +0530

    Comparison of DataType

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//#important = ts can not all to compare two diffrent DataType
+
+// console.log("2" > 1);  // direct js convert("2") stirng to number for comparison 
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// ===
+console.log("2"===2);
