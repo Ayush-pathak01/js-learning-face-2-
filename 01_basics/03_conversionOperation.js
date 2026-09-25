8c2ade399ecd72170794b9edc57d3e67c867321c
aysuh pathak <ayushpathak9389328906@gmail.com>
2026-09-25 08:28:23 +0530

    Converstion of DataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = undefined
+
+console.log(typeof score); // two way to check dataType
+console.log(typeof(score));
+
+
+let valueInNumber =Number(score) // 1 DataType to convert 2 DataTpye 
+
+console.log(typeof valueInNumber);// print DataType of valueInNumber.
+console.log(valueInNumber) // this line just give output of valueInNumber.
+
+
+// "33" => 33
+// "33abc" => NaN
+// true  => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh"  => true
+
+console.log(booleanIsLoggedIn);
+console.log(typeof isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// Convwetion of string DataType
+let someNumber = 33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
