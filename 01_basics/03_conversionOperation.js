4a3feda243ebe2be88cef4076810348a78063f05
aysuh pathak <ayushpathak9389328906@gmail.com>
2026-09-25 09:05:36 +0530

    03_file complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = undefined
 
-console.log(typeof score); // two way to check dataType
-console.log(typeof(score));
+// console.log(typeof score);      // two way to check dataType
+// console.log(typeof(score));
 
 
-let valueInNumber =Number(score) // 1 DataType to convert 2 DataTpye 
+let valueInNumber =Number(score)   // 1 DataType to convert 2 DataTpye 
 
-console.log(typeof valueInNumber);// print DataType of valueInNumber.
-console.log(valueInNumber) // this line just give output of valueInNumber.
+// console.log(typeof valueInNumber); // print DataType of valueInNumber.
+// console.log(valueInNumber)        // this line just give output of valueInNumber.
 
 
 // "33" => 33
@@ -21,21 +21,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" => false
 // "hitesh"  => true
 
-console.log(booleanIsLoggedIn);
-console.log(typeof isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
-// Convwetion of string DataType
+// Conversion of string DataType
 let someNumber = 33
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// *********************** Operation **************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 ="hello "
+let str2="ayush"
+let str3= str1 +str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
 
 
+//prefix or postfix
 
+let a =100
+// ++a //prefix
+// console.log(a);
+
+a++ //postfix
+console.log(a);
 
 
 
