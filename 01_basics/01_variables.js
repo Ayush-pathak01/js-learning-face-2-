4cd0e23f08480a6c13b1f41abe9fea1736e8a002
aysuh pathak <ayushpathak9389328906@gmail.com>
2026-09-25 07:13:04 +0530

    Data Type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail ="ayush"
+var accountPassword="12345"
+accountCity ="jaipur"
+
+
+
+// accountId = 2 // not allowed
+
+accountEmail="had@"
+accountPassword="1023"
+accountCity="etawah"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+/*
+prefre not to use var
+because of issue in block scope and functional scope .
+*/
---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,32 @@
+"use strict";  //treat all JS as a newer version
+
+// alert( 3 + 3)  // We are using node.js not browser .
+
+console.log( 3 + 
+    3)  // code Readability should be high
+
+console.log("ayush")
+
+
+    let name = "ayush"
+    let age = 19
+    let inLoggenIn =false
+    let state ;
+
+// number => 2 to power 53
+// bigint
+// string => "" 
+// boolean => true/faluse
+// null => stand alone value
+//undefine => 
+//symbol => unique
+
+
+//onject
+
+console.log(typeof undefined); // output undefined
+console.log(typeof null);  // output object
+
+
+
+
